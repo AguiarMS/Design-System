d3c65be71eaa93dc59eca11ca9b387b02fbb61b5
vitor <vaguiarms@gmail.com>
2023-01-03 16:43:15 -0300

    Ajustes no arquivo main.cjs  do storybook

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,32 +1,25 @@
-const { config } = require("@storybook/addon-actions");
-
 module.exports = {
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-  ],
-  framework: "@storybook/react",
-  core: {
-    builder: "@storybook/builder-vite",
-  },
-  features: {
-    storyStoreV7: true,
-    interactionsDebugger: true,
-  },
-  viteFinal: (config, { configType }) => {
-    if (configType === "PRODUCTION") {
-      config.base = "/Design-System/";
-    }
-    return config;
-  },
-  "staticDirs": [
-    "../public"
-  ],
-    viteFinal: (config, {configType}) => {
-      if(configType === 'PRODUCTION'){
-        config.base = '/Design-System'
-      }
-    }
+	stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+	addons: [
+		'@storybook/addon-links',
+		'@storybook/addon-essentials',
+		'@storybook/addon-interactions',
+		'@storybook/addon-a11y',
+	],
+	framework: '@storybook/react',
+	core: {
+		builder: '@storybook/builder-vite',
+	},
+	features: {
+		storyStoreV7: true,
+		interactionsDebugger: true,
+	},
+	staticDirs: ['../public'],
+	viteFinal: (config, { configType }) => {
+		if (configType == 'PRODUCTION') {
+			config.base = '/Design-System/';
+		}
+
+		return config;
+	},
 };
